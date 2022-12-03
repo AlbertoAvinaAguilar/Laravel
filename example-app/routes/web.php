39d9e9aea6ef81ed9cf94dbffab0a85d175424fc
AlbertoAvinaAguilar <itecnologia_corporativo@outlook.es>
2022-12-03 13:17:54 -0600

    manejo de rutas diferentes acciones, create, store, show, edit, update, delete

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -19,21 +19,53 @@ Route::get('/', function () {
 });
 
 //Ruta con parametro obligatorio y opcional
-Route::get('saludo/{nombre}/{apellido_materno?}', function($nombre,$apellido_materno = ""){
-    return "Saludo " . $nombre . " " .$apellido_materno;
+Route::get('saludo/{nombre}/{apellido_materno?}', function ($nombre, $apellido_materno = "") {
+    return "Saludo " . $nombre . " " . $apellido_materno;
 });
 
 //Named Routes
-Route::get('usuario', function(){
+Route::get('usuario', function () {
     return "Usuarios";
 })->name('users');
 
-Route::get('detalle', function(){
-    echo "<a href='" . route('users') ."'> Ir a usuarios </a><br>";
+Route::get('detalle', function () {
+    echo "<a href='" . route('users') . "'> Ir a usuarios </a><br>";
 });
 
 //Best practice (Siempre que sea una ruta con nombre)
-Route::get('inicio', function(){
+Route::get('inicio', function () {
     $nombre = "Mayte";
-    return view('inicio')->with('nombre',$nombre);
+    return view('inicio')->with('nombre', $nombre);
 })->name('inicio');
+
+//Best Practice  //Tomar en cuenta el orden al momento de mostrar el formulario y al momentos de buscar por id
+Route::get('productos', function () {
+    return 'aqui iran los productos';
+})->name('productos.index');
+
+Route::get('productos/create', function () { //Mostrar el formulario para crear un producto (siempre primero obigatoriamente 1)
+    return 'aqui mostraremos el formulario para crear los productos';
+})->name('productos.create');
+
+Route::post('productos', function () { //Crear el producto
+})->name('productos.store');
+
+Route::get('productos/{product}', function ($product) { //Obtener un producto por su id (segunda despues de create)
+    return 'Aqui mostraremos el producto con el id seleccionado {$product}';
+})->name('productos.show');
+
+Route::get('productos/{product}/edit', function ($product) { //Obtener un producto por su id
+    return 'Aqui mostraremos el formulario para editar el producto con el id seleccionado {$product}';
+})->name('productos.edit');
+
+Route::match(['put','patch'],'productos/{product}', function ($product) { //Obtener un producto por su id
+
+})->name('productos.update');
+
+Route::delete('productos/{product}', function ($product) { //Obtener un producto por su id
+
+})->name('productos.delete');
+
+
+
+
